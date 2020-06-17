ec1612e73f80793fdb3931598402f7652a630aae
gongitune14 <inoseshouhei@inoseshouheinoMacBook-Air.local>
2020-06-17 10:11:16 +0900

    m

---
+++ db/migrate/20200617011015_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.integer :product_category_id
+      t.string :name
+      t.text :description
+      t.integer :unit_price
+      t.string :product_image_id
+      t.boolean :sales_status
+
+      t.timestamps
+    end
+  end
+end
