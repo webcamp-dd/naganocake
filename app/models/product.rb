27c2f3d74017ca0276e5f379cbfa4b516dfc23b0
vagrant <vagrant@localhost.localdomain>
2020-06-18 05:52:06 +0000

    [Update]suzuki0618-2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
-    has_many :cart_items,dependent: :destroy
+    has_many :cart_product,
+            class_name:  "CartProduct",
+            foreign_key: "product_id",
+            dependent: :destroy
+    has_many :order_products,dependent: :destroy
     
+
 end
