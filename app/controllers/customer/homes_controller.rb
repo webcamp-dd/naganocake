9a3c9be3c759db8d47e2cf4e01607fa8bdec62c8
vagrant <vagrant@localhost.localdomain>
2020-06-18 05:51:24 +0000

    6/18 start

--- app/controllers/customer/homes_controller.rb
+++ app/controllers/customer/homes_controller.rb
@@ -1,5 +1,7 @@
 class Customer::HomesController < ApplicationController
   def top
-    @product_categories = ProductCategory.all
+    # @product_categories = ProductCategory.all
+    # @product_category = ProductCategory.find(params[:id])
+    # @product_categories = @product_category.products
   end
 end
--- app/controllers/customer/products_controller.rb
+++ app/controllers/customer/products_controller.rb
@@ -1,13 +1,24 @@
 class Customer::ProductsController < ApplicationController
   def index
+    @products = Product.page(params[:page]).reverse_order
+    @product = Product.new
   end
 
+# @category = ProductCategory.find(params[:id])
+# @category = ProductCategory.new
+
   def show
-    @category = ProductCategory.find(params[:id])
+    
+  end
+
+  def create
+    product = Product.new(product_params)
+    redirect_to customer_products_index_path
   end
 
   private
 	  	def product_params
-		 	params.require(:product).permit(:name, :description, :unit_price, :product_image_id, :sales_status)
-	  	end
+		 	params.require(:product).permit(:name, :description, :unit_price, :product_image, :sales_status)
+      end
+    
 end
