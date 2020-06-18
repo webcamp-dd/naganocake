dc94dccdd0af5476014dc59afdff8cb692800d80
vagrant <vagrant@localhost.localdomain>
2020-06-18 01:17:11 +0000

    6/18s

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -13,4 +13,10 @@ class Customer::CustomersController < ApplicationController
 
   def delete_confimation
   end
+
+  private
+	  	def customer_params
+		 	params.require(:customer).permit(:name, :family_name, :first_name, :kana_family_name, :kana_first_name, :postal_code, :address, :phone_number, :valid)
+	end
+
 end
