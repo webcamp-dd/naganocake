8457d05d606be454d89a35b08f59c51b6521c36e
vagrant <vagrant@localhost.localdomain>
2020-06-20 02:05:52 +0000

    6/20 sho

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
 class Product < ApplicationRecord
+<<<<<<< Updated upstream
     has_many :cart_items,dependent: :destroy
     attachment :product_image
+=======
+    has_many :cart_product,
+            class_name:  "CartProduct",
+            # foreign_key: "product_id",
+            dependent: :destroy
+    has_many :order_products,dependent: :destroy
+    attachment :product_image
+    belongs_to :product_category
+>>>>>>> Stashed changes
 end
