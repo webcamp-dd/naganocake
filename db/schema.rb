86c02b8a7810f2a0a65424e18d26771751beb372
vagrant <vagrant@localhost.localdomain>
2020-06-20 12:10:38 +0000

    [Update]0620-2-suzuki

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_19_075423) do
+ActiveRecord::Schema.define(version: 2020_06_20_120840) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 2020_06_19_075423) do
   end
 
   create_table "deliveries", force: :cascade do |t|
-    t.integer "user_id"
+    t.integer "customer_id"
     t.string "postal_code"
     t.string "address"
     t.string "name"
