3dc4dbc51a2ccbe0fb84ae7f1becfa087b291c19
vagrant <vagrant@localhost.localdomain>
2020-06-21 12:56:38 +0000

    会員画面、注文一覧、トップ画面機能実装

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,11 +1,14 @@
 class Admin::CustomersController < ApplicationController
   def index
+  	@customers = Customer.page(params[:page])
   end
 
   def show
+  	@customer = Customer.find(params[:id])
   end
 
   def edit
+  	@customer = Customer.find(params[:id])
   end
 
   def update
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,14 +1,20 @@
 class Admin::OrdersController < ApplicationController
+
   def index
+  	@orders = Order.page(params[:page])
   end
 
   def show
+  	@order = Order.find(params[:id])
+  	@order_customer = @order.customer
+  	# @delivery = 
+  	@order_product = @order.order_products
+
   end
-  
+
   def new
   end
 
-  
   def update
   end
 end
