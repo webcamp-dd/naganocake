e76c5f5e74fb7afbcc5df50bf576aab5357ef8d4
vagrant <vagrant@localhost.localdomain>
2020-06-23 08:52:20 +0000

    add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-    
-    # def after_admin_sign_out_path_for(resource)
-    #   new_admin_session_path # ログアウト後に遷移するpathを設定
-    # end
+      
+      def after_sign_out_path_for(a)
+        if a == :admin
+          new_admin_session_path
+        end
+      end
   protected
 
   def configure_permitted_parameters
